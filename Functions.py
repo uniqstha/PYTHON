7fca52797abd99c8ecd8e439feb52237cd9df31f
uniqstha <unique2059@gmail.com>
2021-06-16 10:14:22 +0545

    Function of python example is added

---
+++ Functions.py
@@ -0,0 +1,22 @@
+#Function in python example
+
+def add():
+    print('Function of add start')
+    a = int(input('Enter first number'))
+    b = int(input('Enter second number'))
+    c = a + b
+    print(f'The sum of {a} and {b} is {c}')
+    print('Function of add ends')
+
+add()
+
+
+def sub():
+    print('Function of sub start')
+    a = int(input('Enter first number'))
+    b = int(input('Enter second number'))
+    c = a - b
+    print(f'The sub of {a} and {b} is {c}')
+    print('Function of sub ends')
+sub()
+print('program end')
